9a37a3180308bec6053ba82b7e92fd9b6345e0d3
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-03-28 18:46:41 +0530

    state

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from 'react'; 
 
 class CartItem extends React.Component{
+    constructor () {
+        super();
+        this.state = {
+            price:999,
+            title:'Phone',
+            qty: '1',
+            img: ''
+        }
+    }
     render() {
+        const {price,title,qty} =this.state;
         return (
             <div className="cart-item">
                 <div className="left-block" >
                 <img style={styles.image} />
                 </div>
                 <div  className="right-block" >
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color:'#777'}}>Rs 999</div>
-                    <div style={{color:'#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color:'#777'}}>{price}</div>
+                    <div style={{color:'#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions" >
                             {/* Button */}
+                            <img alt="increase" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1616937699~hmac=f9662fd55671d8c6079edaf185030f51"/>
+                            <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/104/104616.svg?token=exp=1616937726~hmac=ff9a3a660573adfa5fda03ae57dd51c2"/>
+                             <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1616937756~hmac=61beee9c0e20107ab9c328c35b529996"/>
                     </div>
                 </div>
             </div>
