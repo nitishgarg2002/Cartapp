92b488a30fbd7c0a7fd692771b8752772d5c0689
nitishgarg2002 <nitishgarg2002@gmail.com>
2021-04-14 14:32:39 +0530

    frontenddone

--- src/App.js
+++ src/App.js
@@ -1,12 +1,100 @@
 import React from 'react';
-import CartItem from './CartItem';
+import Navbar from './Navbar';
+import Cart from './Cart';
 
-function App() {
-  return (
-    <div className="App">
-     <CartItem/>
-    </div>
+class App  extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+        products: [
+            {
+                price: 999,
+                title: 'Phone',
+                qty: 1,
+                img: '',
+                id: 1
+            },
+            {
+                price: 9999,
+                title: 'Tv',
+                qty: 10,
+                img: '',
+                id: 2
+            },
+            {
+                price: 99999,
+                title: 'Laptop',
+                qty: 4,
+                img: '',
+                id: 3
+            }
+        ]
+    }
+}
+handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    products[index].qty += 1;
+    this.setState({
+        products
+    })
+}
+handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+    if (products[index].qty > 0) {
+        products[index].qty -= 1;
+        this.setState({
+            products
+        })
+    }
+
+}
+handleDeleteProduct = (id) => {
+    const { products } = this.state;
+    const items = products.filter((item) => item.id !== id);
+    this.setState({
+        products: items
+    })
+}
+getCartCount = ()=>{
+  const {products} = this.state;
+  let count=0;
+  products.forEach( 
+    (product)=> {
+      count+=product.qty;
+    }
   );
+  return count;
+}
+getCartTotal=() => {
+  const {products} = this.state;
+  let cartTotal = 0;
+  products.map((product)=> {
+    cartTotal= cartTotal+ product.qty* product.price;
+  })
+  return cartTotal;
+}
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart 
+        products={products}
+        onIncreaseQuantity={this.handleIncreaseQuantity}
+        onDecreaseQuantity={this.handleDecreaseQuantity}
+        onDeleteProduct={this.handleDeleteProduct}
+  
+        />
+        <div style={ {padding :10, fontSize:20}} >
+          Total: {this.getCartTotal()}
+        </div>
+      </div>
+
+    );
+  }
+  
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,39 +1,36 @@
-import React from 'react'; 
+import React from 'react';
 
-class CartItem extends React.Component{
-    constructor () {
-        super();
-        this.state = {
-            price:999,
-            title:'Phone',
-            qty: '1',
-            img: ''
-        }
-      //  this.increaseQuantity = this.increaseQuantity.bind(this);
-    }
-    increaseQuantity =()=> {
-        console.log(this.state);
-    }
+class CartItem extends React.Component {
+
     render() {
-        const {price,title,qty} =this.state;
+
+        const { price, title, qty } = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block" >
-                <img style={styles.image} />
+                    <img alt="" style={styles.image} />
                 </div>
-                <div  className="right-block" >
-                    <div style={{fontSize:25}}>{title}</div>
-                    <div style={{color:'#777'}}>{price}</div>
-                    <div style={{color:'#777'}}>Qty: {qty}</div>
+                <div className="right-block" >
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: '#777' }}>{price}</div>
+                    <div style={{ color: '#777' }}>Qty: {qty}</div>
                     <div className="cart-item-actions" >
-                            {/* Button */}
-                            <img alt="increase" 
-                            className="action-icons" 
-                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1616937699~hmac=f9662fd55671d8c6079edaf185030f51"
-                            onClick={this.increaseQuantity}
-                            />
-                            <img alt="decrease" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/104/104616.svg?token=exp=1616937726~hmac=ff9a3a660573adfa5fda03ae57dd51c2"/>
-                             <img alt="delete" className="action-icons" src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1616937756~hmac=61beee9c0e20107ab9c328c35b529996"/>
+                        {/* Button */}
+                        <img alt="increase"
+                            className="action-icons"
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992651.svg?token=exp=1617959181~hmac=9a894fa33e947907036169e21839befd"
+                            onClick={() => this.props.onIncreaseQuantity(this.props.product)}
+                        />
+                        <img
+                            alt="decrease"
+                            className="action-icons"
+                            src="https://www.flaticon.com/svg/vstatic/svg/992/992683.svg?token=exp=1617959273~hmac=365343c586bfab07ebe0dabbee04ea7f"
+                            onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+                        />
+                        <img alt="delete" className="action-icons"
+                            src="https://www.flaticon.com/svg/vstatic/svg/1214/1214428.svg?token=exp=1617959212~hmac=729ad9f4166bafc4968cf4274143322d"
+                            onClick={() => this.props.onDeleteProduct(this.props.product.id)}
+                        />
                     </div>
                 </div>
             </div>
@@ -45,7 +42,7 @@ const styles = {
     image: {
         height: 110,
         width: 110,
-        borderRadius:4,
+        borderRadius: 4,
         background: '#ccc'
     }
 }
